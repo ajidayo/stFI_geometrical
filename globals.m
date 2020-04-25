dff7144ad1dcb15958ec3aafc3d8a05fe01e8b8f
ajidayo <46882529+ajidayo@users.noreply.github.com>
2020-04-25 13:05:07 +0900

    Add files via upload

---
+++ globals.m
@@ -0,0 +1,12 @@
+global FNum ENum NNum PNum
+
+global EPSILON
+global dim
+global denominator_obi
+global Size_X Size_Y
+global Fine_Y_from Fine_Y_to
+global Triangle_Y_from Triangle_Y_to
+
+global deltatriangle
+global deltaboundary
+
